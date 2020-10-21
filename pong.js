99dda3d9e08bb29e065dd23156a7a6289f7adbbf
ToddTeese <todd.teese@colorconsultancy.co.uk>
2020-10-21 18:42:20 +0100

    pong canvas

--- pong.js
+++ pong.js
@@ -1,1 +1,10 @@
 console.log("Pong App Loaded!");
+
+const canvas = document.querySelector('#gameArea');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 640;
+canvas.height = 480;
+
+ctx.fillStyle = '#000';
+ctx.fillRect(0,0, canvas.width, canvas.height);
