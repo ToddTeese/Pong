2d00f56cfe15b389f29cc1e9794d40ffe8427e1d
ToddTeese <todd.teese@colorconsultancy.co.uk>
2020-10-21 18:49:19 +0100

    ball added

--- pong.js
+++ pong.js
@@ -8,3 +8,37 @@ canvas.height = 480;
 
 ctx.fillStyle = '#000';
 ctx.fillRect(0,0, canvas.width, canvas.height);
+
+// player
+
+// computer player
+
+// ball
+
+//
+
+class Actor {
+
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Ball extends Actor {
+    constructor(x, y, size, color) {
+        super(x, y, size, size, color);
+    }
+}
+
+const ball = new Ball(canvas.width / 2, canvas.height / 2, 16, '#FFF');
+
+ball.draw(ctx);
